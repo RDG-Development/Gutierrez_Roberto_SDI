82c414ea5f731067d6707c777609341860c336e0
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-10 16:15:25 -0700

    Following along Arrays
    Following along Arrays video and using console.log in order to bring up
    specific values within the array index

--- Associating Java Script/JS/script.js
+++ Associating Java Script/JS/script.js
@@ -1,11 +1,5 @@
-// Variables of Different Types, Numbers, Booleans and Strings
+// Arrays
 
+var muppetNames = ["Kermit", "Miss Piggy", "Gonzo", "Rizzo"]; //0,1,2,3
 
-var age = 24; // Number Variable
-
-var isStudent = false; //Boolean Variable
-
-
-var firstName = "Kermit"; //String Variable
-
-var phrase = 'I don\'t know \n You\'ll have to speak with the Professor';
+console.log(muppetNames[0]);
