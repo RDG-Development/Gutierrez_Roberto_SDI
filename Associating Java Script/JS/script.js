f528affe763883aaa7721689db1209b90c84648c
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-11 00:04:39 -0700

    Created an Example of a Calc
    Created an example calculation with the answer inside an alert box

--- Associating Java Script/JS/script.js
+++ Associating Java Script/JS/script.js
@@ -1,8 +1,8 @@
 // Chapter 2_examp3.html
-<//!DOCTYPE html PUBLIC >
+//!DOCTYPE html PUBLIC >
 var firstNumber = 15;
 var secondNumber = 10;
 var answer;
 answer = 15 / 10;
 
-alert(answer)
+alert(answer);
