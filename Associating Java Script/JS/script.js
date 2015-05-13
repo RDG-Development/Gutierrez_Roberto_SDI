fb71e36cbc539f0f081cfbbe744443dd560862cc
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-12 22:46:40 -0700

    Observed difference between numbers and strings in Concatenation
    Practiced concatenation and noticed the difference between combining
    strings with the + operator or numbers with that same operator.

--- Associating Java Script/JS/script.js
+++ Associating Java Script/JS/script.js
@@ -4,6 +4,21 @@ var firsName = "Roberto";
 
 var lastName = "Gutierrez";
 
-var fullName = firsName + lastName;
+var fullName = firsName + " " + lastName;
 
 console.log(fullName);
+
+
+
+var a = "6";
+
+var b = "7";
+
+var result = a + b;
+console.log(result);
+
+// gives 67 instead of 13, this is because
+//a String only holds the characters 6 & 7
+//it is not the same as adding the numbers 6 &
+//7. Strings and Numbers are Not the same
+
