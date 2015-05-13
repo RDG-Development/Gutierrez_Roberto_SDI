ba1a386544024ca2afcb11c292db831c30cd40dd
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-13 13:06:56 -0700

    Practiced Expressions,
    Practiced using expressions, operators, prompts, and alerts to use user
    input to display data

--- Associating Java Script/JS/script.js
+++ Associating Java Script/JS/script.js
@@ -1,21 +1,13 @@
-//Expression - Assignment Operators
-
-var a = 3;
-
-a += 4; //a = a + 4
-
-console.log(a);
-
-var b = 2;
-
-b ++ ; // b = b + 1 // or b += 1
-
-b -- ; // b = b - 1 // or b -= 1
-
-console.log(b);
+//Prompt and Alert
 
 
 
+var userInput = prompt("Enter your year of birth");
+console.log(userInput)
 
+var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width");
+var height = prompt("Please enter the height");
 
-
+var area = width * height; //calculates with the informationg entered into the prompts
+var result = "The area of your rectangle is" + " " + area + " " + "sq. ft.";
+alert(result);
