031eb776e653fcf39738cf3aa66a1755ea29b124
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-13 19:19:02 -0700

    Created problem 1
    Created a solution to problem one through prompts and alerts

--- Expressions_worksheet/expressionsJS/expScript.js
+++ Expressions_worksheet/expressionsJS/expScript.js
@@ -1,4 +1,47 @@
-//Roberto Gutierrez 5/13/2015 Expressions Worksheet
+/*Roberto Gutierrez
+ 5/13/2015
+ SDI Section 01
+ Expressions Worksheet
+  */
+
+//Problem 1  Find Spark's age in Dog Years
+
+//Enter the age of Sparky in Human years when prompted.
+var sparkysHumanAge = Number(prompt("How old is Sparky in human years?"));
+
+
+//How many times more does a dog age than a human each year?
+var dogAgesToHumans = 7 * 1
+
+//Multiply Sparkys human age by the dogAgesToHumans variable.
+var sparkysDogAge = dogAgesToHumans * sparkysHumanAge;
+
+//Alert user of Sparky's age in dog years.
+alert("Sparky is " + sparkysDogAge + " dog years old.")
+
+//print out sparkysDogAge
+console.log(sparkysDogAge + " Years Old");
+
+/* I entered in 5 when prompted,
+The result I get is 35
+This answer is correct
+ */
+
+//Problem 2
+/* How much pizza will each party attendee get?
+ */
+
+var numberOfSlicesPerPizza = Number(prompt("How many slices of pizza does each pizza have?"));
+
+console.log(numberOfSlicesPerPizza);
+
+
+
+
+
+
+
+
 
 
 
