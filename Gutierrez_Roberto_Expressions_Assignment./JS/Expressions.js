bb552f0562b228489c70d3a2c7881def54b39ea0
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-14 18:59:47 -0700

    Added more text
    Added more descriptive text into prompts

--- Gutierrez_Roberto_Expressions_Assignment./JS/Expressions.js
+++ Gutierrez_Roberto_Expressions_Assignment./JS/Expressions.js
@@ -13,11 +13,11 @@ alert("You, are a Battle-Mage in the middle of a war zone.");
 
 alert("A fearsome warrior spots you and begins to charge, with a sword raised to strike you down.");
 
-alert("You have 3 spells at your disposal, but you only have enough energy to cast one.");
+alert("You have 3 spells at your disposal.");
 
-alert("Choose which spell with which to destroy this fool who dares to charge you!");
+alert("Choose which spell or spells with which to destroy this fool who dares to charge you!");
 
-alert("Remember you can only choose one to cast, for even if you get a value, you cannot cast more than one choice in theory.")
+alert("Remember you can cast one of these spells or all three.")
 
 
 
@@ -26,10 +26,10 @@ alert("Remember you can only choose one to cast, for even if you get a value, yo
 var userChose =[Number(prompt("Will you choose a FireBolt spell? Type 1 for yes or 0 for no")), Number(prompt("Will you choose a Icebolt spell? Type 1 for yes or 0 for no")),Number(prompt("Will you choose a DeathBolt spell? Type 1 for yes or 0 for no"))]
 
 //Give a prompt to let the user describe the spell
-var describeSpell = prompt("What spell did you choose? A FireBolt, a IceBolt, or a DeathBolt?")
+var describeSpell = prompt("What spell did you choose? FireBolt, IceBolt, or DeathBolt? If you chose all three it would look like this. \'FireBolt, IceBolt, and DeathBolt\'")
 
 //Give a prompt to let the user describe the effects of the spell
-var effectsOfSpell = prompt("Type the phrase that best fits your spell, (Swallowed and Vaporized),(Frozen and Shattered), (Poisoned and Rotted))")
+var effectsOfSpell = prompt("Type the phrase that best fits your spell, (Swallowed and Vaporized),(Frozen and Shattered), (Poisoned and Rotted). Type \'and\' between each phrase if you chose more than one. If you chose more than one type a comma between each phrase. Like this \'Swallowed and Vaporized,Frozen and Shattered, and Poisoned and Rotted\'.")
 
 
 //Create an Array of your spells
@@ -49,7 +49,7 @@ var fireBoltSpellTotalDamage = fireBoltImpactDamage + fireBoltDamagePerSecondFor
 spell[0] = fireBoltSpellTotalDamage;
 
 //Test if your value appears on the console.
-console.log(spell[0]);
+console.log(spell[0] + " Total FireBolt Damage");
 
 //Create a variable for the damage an icebolt does.
 var iceBoltImpactDamage = 150;
@@ -64,7 +64,7 @@ var iceBoltSpellTotalDamage = iceBoltImpactDamage + iceBoltDamagePerSecondForThr
 spell[1] = iceBoltSpellTotalDamage
 
 //Test if your value appears on the console.
-console.log(spell[1]);
+console.log(spell[1] + " Total IceBolt Damage");
 
 //Create a variable for the damage a deathbolt does.
 var deathBoltImpactDamage = 25;
@@ -79,14 +79,14 @@ var deathBoltSpellTotalDamage = deathBoltImpactDamage + deathBoltDamagePerSecond
 spell[2] = deathBoltSpellTotalDamage
 
 //Test if your value appears on console.
-console.log(spell[2])
+console.log(spell[2] + " Total Death Bolt Damage")
 
 //Calculate the total damage from the users input.
 
 Damage = (userChose[0] * spell[0]) + (userChose[1] * spell[1]) + (userChose[2] * spell[2])
 
 //Test to see if it appears on console.
-console.log(userChose);
+console.log(userChose + " ....... Number of FireBolt Spell Cast, Number of IceBolt Spell Cast, Number of DeathBolt Spell Cast, Respectfully");
 
 //alert the user of their choices.
 alert("Your " + describeSpell + " has " + effectsOfSpell + " your enemy, leaving the warriors\' unrecognizable remains on the charred battlefield. Pity the next fool that dares challenge you! You have caused " + Damage + " damage! You retreat to a safer area so that your energy can replenish, and so you can return to the glorious battlefield.");
