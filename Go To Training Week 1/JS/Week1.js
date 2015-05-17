400ebfe8bfcd45a3425c3ef6126f0b6f59e3f4b4
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-17 13:33:18 -0700

    Practicing Conditionals
    Practicing Conditionals while following videos.

--- Go To Training Week 1/JS/Week1.js
+++ Go To Training Week 1/JS/Week1.js
@@ -6,3 +6,41 @@ GoTo Training Week 1
  */
 
 //alert("Testing to see if connected.");
+
+
+/*First create your prompts, and put a comma between each prompt
+in order to make separate values in your array.
+Next enter Number in front of each prompt and make sure to put
+parenthesis in before the prompt and after the prompt as shown
+below.
+ */
+var myArray = [Number(prompt("How old are you?")), Number(prompt("How old is your brother?")), Number(prompt("How old is your sister?"))]
+
+
+/*You now have an array, and each different value
+can be accessed through array notation like so,
+myArray[0], myArray[1], myArray[2]
+
+/* You can now create a variable for total
+siblings
+ */
+
+var totalSiblings = 3
+
+/*You can now get the average age
+of the siblings by using array notation and
+the through the use of operators.
+ */
+
+var averageSiblingAge = (myArray[0] + myArray[1] + myArray[2]) / totalSiblings
+
+/*console/.log to check your work
+ */
+
+console.log(averageSiblingAge)
+
+/* if it shows up in the console, then you are ready to release an alert
+
+ */
+
+alert(averageSiblingAge)
---
+++ Training/JS/mytraining.js
@@ -0,0 +1,15 @@
+/**
+ * Created by NicaDevelopment on 5/17/15.
+ */
+//alert("Testing Connection")
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console, "you can ride"
+
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+        }else{console.log("You cannot ride the coaster! Sorry Loser!")}
+
+
