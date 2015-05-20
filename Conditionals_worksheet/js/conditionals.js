d26c519b0c9e0349d1eef19d471fd508b332678d
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-19 23:02:42 -0700

    Created first part of Problem 1
    Created an explantation alert for user, and create variables for
    fahrenheit and celsius.

--- Conditionals_worksheet/js/conditionals.js
+++ Conditionals_worksheet/js/conditionals.js
@@ -5,6 +5,19 @@ SDI
 section 001
 Conditionals_worksheet
  */
+//Problem 1
 
+/*Convert a temperature to either degrees Celsius or degrees Fahrenheit depending on what the user has entered. If the user puts a “C” for the unit, the calculator should convert to Celsius. If the user puts “F” for the unit, the calculator should convert to Fahrenheit.
+ */
+
+//Alert the user what the following will do.
+
+alert("The following pop-up windows will assist you in converting temperature from/to Fahrenheit or from/to Celsius depending on what you want to do.")
+
+//Create variables for fahrenheit and ceslsius
+
+var fahrenheit = "F"
+
+var celsius = "C"
 
 
