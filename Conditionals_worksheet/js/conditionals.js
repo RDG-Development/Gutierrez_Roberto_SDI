27af61d0132394d2d76b49847baae5d566ca0388
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-20 01:28:26 -0700

    Created another alert and more
    Created another alert and added console.log and some description of
    actions

--- Conditionals_worksheet/js/conditionals.js
+++ Conditionals_worksheet/js/conditionals.js
@@ -12,12 +12,22 @@ Conditionals_worksheet
 
 //Alert the user what the following will do.
 
-alert("The following pop-up windows will assist you in converting temperature from/to Fahrenheit or from/to Celsius depending on what you want to do.")
+alert("The following pop-up windows will assist you in converting temperature from/to Fahrenheit or from/to Celsius depending on what you want to do.");
+
+
+//Create a variable that prompts the user how many degrees they want to convert.
+
+var howManyDegrees = prompt("What is the temperature that you want to convert from? Example:Type the only the number ( 100 ) for 100 degrees fahrenheit or celsius");
+
+console.log(howManyDegrees);
 
 //Create variables for fahrenheit and ceslsius
+var fahrenheit = "F";
 
-var fahrenheit = "F"
+var celsius = "C";
 
-var celsius = "C"
+//Create a prompt that asks the user what they want to convert to.
+
+var converTo = prompt
 
 
