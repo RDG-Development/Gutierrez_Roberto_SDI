b5a7b6517372f050d1d6318cc15ffb8fa8413085
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-24 21:00:54 -0700

    Practiced the difference between functions and procedures.

--- Training/JS/mytraining.js
+++ Training/JS/mytraining.js
@@ -3,14 +3,27 @@
  */
 //alert("Testing Connection")
 
+
+
 function calcArea(w,h){
     var area = w * h;
     return area
+    console.log(area);
 };
+
 
 var userRectangleArea = calcArea(Number(prompt("What is the width of your rectangle?")), Number(prompt("What is the length of your rectangle?")));
 
 console.log(userRectangleArea);
 
 alert("Your rectangle area is, " + userRectangleArea + " units.");
+
+//This is a procedure below. It is a function, but does not return a value. IT just performs a list of instructions that are coded into it.
+
+function calcArea(w,h){
+    var area = w * h;
+    console.log(area);
+};
+
+
 
