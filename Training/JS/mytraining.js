9dd14f9fe8c70fb35ad5daac92694ed64495bed5
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-24 20:52:03 -0700

    Created my own example of the Practice
    I added prompts to the example of functions in the videos in order to
    make the function very flexible to the users need to get rectangle
    area.

--- Training/JS/mytraining.js
+++ Training/JS/mytraining.js
@@ -3,32 +3,14 @@
  */
 //alert("Testing Connection")
 
- function writeUserWelcome(userName, userAge){
-    console.log("Welcome to my website " + userName + ".")
-    console.log("You are " + userAge + " years old.")
-    console.log("I hope you enjoy my website.")
+function calcArea(w,h){
+    var area = w * h;
+    return area
 };
 
-writeUserWelcome(prompt("What is your name?"), Number(prompt("What is your age?")));
+var userRectangleArea = calcArea(Number(prompt("What is the width of your rectangle?")), Number(prompt("What is the length of your rectangle?")));
 
-function convertToCentigrade(degFahren)
-{
-    var degCent;
-    degCent = 5/9 * (degFahren - 32);
+console.log(userRectangleArea);
 
-    return degCent;
-}
-var degFahren = new Array(212, 32, -459.15);
-var degCent = new Array();
-var loopCounter;
-for (loopCounter = 0; loopCounter <= 2; loopCounter++)
-{
-    degCent[loopCounter] = convertToCentigrade(degFahren[loopCounter]);
-}
-for (loopCounter = 2; loopCounter >= 0; loopCounter-- )
-{
-    console.log("Value " + loopCounter +  " was " + degFahren[loopCounter] +
-                  " degrees Fahrenheit");
-    console.log( "Which is " + degCent[loopCounter] +
-                    " degrees centigrade");
-}
+alert("Your rectangle area is, " + userRectangleArea + " units.");
+
