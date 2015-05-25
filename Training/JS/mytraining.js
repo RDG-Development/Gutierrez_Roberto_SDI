3f56f980d0fcd4963e45d20ffb464f61e9259b53
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-24 19:38:20 -0700

    Practiced Functions

--- Training/JS/mytraining.js
+++ Training/JS/mytraining.js
@@ -3,11 +3,32 @@
  */
 //alert("Testing Connection")
 
-    alert("The following prompts will inform you if you are allowed to ride this roller coaster, or not.")
+ function writeUserWelcome(userName, userAge){
+    console.log("Welcome to my website " + userName + ".")
+    console.log("You are " + userAge + " years old.")
+    console.log("I hope you enjoy my website.")
+};
 
-var minHeightFeet = 4.5 ;
+writeUserWelcome(prompt("What is your name?"), Number(prompt("What is your age?")));
 
-var yourHeightInches = Number(prompt("What is your Height? Write your height as follows: Five foot, eleven inches would be written as (5.11)"));
+function convertToCentigrade(degFahren)
+{
+    var degCent;
+    degCent = 5/9 * (degFahren - 32);
 
-if(yourHeightInches >= minHeightFeet){alert( "You are allowed to ride this coaster.")
-    } else{alert("You are not allowed to ride this coaster.")}
+    return degCent;
+}
+var degFahren = new Array(212, 32, -459.15);
+var degCent = new Array();
+var loopCounter;
+for (loopCounter = 0; loopCounter <= 2; loopCounter++)
+{
+    degCent[loopCounter] = convertToCentigrade(degFahren[loopCounter]);
+}
+for (loopCounter = 2; loopCounter >= 0; loopCounter-- )
+{
+    console.log("Value " + loopCounter +  " was " + degFahren[loopCounter] +
+                  " degrees Fahrenheit");
+    console.log( "Which is " + degCent[loopCounter] +
+                    " degrees centigrade");
+}
