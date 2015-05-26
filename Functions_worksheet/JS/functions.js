5fb193e2d252bc4586f1af30ba9460b2577ab1f4
Roberto Gutierrez <xlrgutierrezlx@yahoo.com>
2015-05-26 01:52:49 -0700

    created a variable to hold the user sting calculation result.

--- Functions_worksheet/JS/functions.js
+++ Functions_worksheet/JS/functions.js
@@ -73,3 +73,5 @@ var beeStingCalculation = function(p){
 var userPoundsInput = Number(prompt("What is the weight of the animal or human in pounds? You can put your own wight.\nOnly enter a number value."));
 
 
+var userStingValue = beeStingCalculation(userPoundsInput);
+
